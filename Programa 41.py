cb314ac9a503fb20127b799bd4be84466d1e68e9
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-09-24 21:35:32 -0300

    Programa 41
    Programa 41

---
+++ Programa 41.py
@@ -0,0 +1,14 @@
+# Programa 41
+
+a = int(input('Primeiro valor. '))
+b = int(input('Segundo valor. '))
+
+if a > b:
+    print('O primeiro é maior. ')
+    
+if b > a:
+    print('O segundo é maior. ') 
+    
+if a == b:
+    print('Os números são iguais. ')
+    
